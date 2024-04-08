430ecd568ff313376672a5dba2f2698e71ba49be
Matilde Simões <up202108782@up.pt>
2024-04-08 11:02:13 +0100

    clean imports in semantic files

--- src/main/pt/up/fe/comp2024/analysis/passes/ClassNotImported.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/ClassNotImported.java
@@ -2,11 +2,8 @@ package pt.up.fe.comp2024.analysis.passes;
 
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.JmmNode;
-import pt.up.fe.comp.jmm.report.Report;
-import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.comp2024.analysis.AnalysisVisitor;
 import pt.up.fe.comp2024.ast.Kind;
-import pt.up.fe.comp2024.ast.NodeUtils;
 
 public class ClassNotImported extends AnalysisVisitor {
 
--- src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredVariable.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredVariable.java
@@ -2,18 +2,10 @@ package pt.up.fe.comp2024.analysis.passes;
 
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.JmmNode;
-import pt.up.fe.comp.jmm.report.Report;
-import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.comp2024.analysis.AnalysisVisitor;
 import pt.up.fe.comp2024.ast.Kind;
-import pt.up.fe.comp2024.ast.NodeUtils;
 import pt.up.fe.specs.util.SpecsCheck;
 
-/**
- * Checks if the type of the expression in a return statement is compatible with the method return type.
- *
- * @author JBispo
- */
 public class UndeclaredVariable extends AnalysisVisitor {
 
     private String currentMethod;

12fe5250c25df7a26a4bb028f1e65d8736fd9452
Matilde Simões <up202108782@up.pt>
2024-05-22 23:36:28 +0100

    tentative refactor constant propagation

--- src/main/pt/up/fe/comp2024/optimization/OllirConstantPropagationVisitor.java
+++ src/main/pt/up/fe/comp2024/optimization/OllirConstantPropagationVisitor.java
@@ -5,22 +5,143 @@ import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp2024.ast.Kind;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class OllirConstantPropagationVisitor extends AJmmVisitor<Void, Boolean> {
-    private HashSet<String> forbidden;
-    private ArrayList<JmmNode> propagatableNodes;
+    private final HashMap<String, JmmNode> globalConstants = new HashMap<>();
+
 
     @Override
     protected void buildVisitor() {
         addVisit(Kind.METHOD_DECL, this::visitMethodDecl);
-        addVisit(Kind.ASSIGN_STMT, this::visitAssignStmt);
+        addVisit(Kind.STMT_BLOCK, this::visitStmtBlock);
         addVisit(Kind.IF_ELSE_STMT, this::visitIfElseStmt);
         addVisit(Kind.WHILE_STMT, this::visitWhileStmt);
+        addVisit(Kind.SIMPLE_STMT, this::visitSimpleStmt);
+        addVisit(Kind.ASSIGN_STMT, this::visitAssignStmt);
         addVisit(Kind.VAR_REF_EXPR, this::visitVarRefExpr);
         setDefaultVisit(this::defaultVisit);
     }
 
+    private boolean visitMethodDecl(JmmNode methodDecl, Void unused) {
+        boolean propagated = false;
+
+        for (JmmNode stmt : methodDecl.getChildren()) {
+            propagated = visit(stmt) || propagated;
+        }
+
+        for (JmmNode constant : this.globalConstants.values()) {
+            constant.removeParent();
+        }
+
+        return propagated;
+    }
+
+    private boolean visitStmtBlock(JmmNode stmtBlock, Void unused) {
+        boolean propagated = false;
+        ArrayList<String> localConstants = new ArrayList<>();
+
+        for (JmmNode stmt : stmtBlock.getChildren()) {
+            if (stmt.getKind().equals(Kind.ASSIGN_STMT.toString())) {
+                JmmNode expr = stmt.getChild(0);
+
+                if (expr.getKind().equals(Kind.INTEGER_LITERAL.toString()) || expr.getKind().equals(Kind.BOOLEAN_LITERAL.toString())) {
+                    String name = stmt.get("name");
+                    this.globalConstants.put(name, expr);
+                    localConstants.add(name);
+                    expr.removeParent();
+                }
+            }
+
+            for (JmmNode child : stmt.getChildren()) {
+                propagated = visit(child) || propagated;
+            }
+        }
+
+        for (String constant : localConstants) {
+            this.globalConstants.get(constant).removeParent();
+            this.globalConstants.remove(constant);
+        }
+
+        return propagated;
+    }
+
+    private boolean visitIfElseStmt(JmmNode ifElseStmt, Void unused) {
+        JmmNode expr = ifElseStmt.getChild(0);
+        boolean propagated = visit(expr);
+
+        for (JmmNode ifStmtChild : ifElseStmt.getChild(1).getChildren()) {
+            propagated = visit(ifStmtChild) || propagated;
+
+            if (ifStmtChild.getKind().equals(Kind.ASSIGN_STMT.toString())) {
+                String name = ifStmtChild.get("name");
+                this.globalConstants.remove(name);
+            }
+        }
+
+        for (JmmNode elseStmtChild : ifElseStmt.getChild(2).getChildren()) {
+            propagated = visit(elseStmtChild) || propagated;
+
+            if (elseStmtChild.getKind().equals(Kind.ASSIGN_STMT.toString())) {
+                String name = elseStmtChild.get("name");
+                this.globalConstants.remove(name);
+            }
+        }
+
+        return propagated;
+    }
+
+    private boolean visitWhileStmt(JmmNode whileStmt, Void unused) {
+        boolean propagated = false;
+
+        for (JmmNode whileStmtChild : whileStmt.getChild(1).getChildren()) {
+            if (whileStmtChild.getKind().equals(Kind.ASSIGN_STMT.toString())) {
+                String name = whileStmtChild.get("name");
+                this.globalConstants.remove(name);
+            }
+
+            propagated = visit(whileStmtChild) || propagated;
+        }
+
+        JmmNode expr = whileStmt.getChild(0);
+        return visit(expr) || propagated;
+    }
+
+    private boolean visitSimpleStmt(JmmNode simpleStmt, Void unused) {
+        return visit(simpleStmt.getChild(0));
+    }
+
+    private boolean visitAssignStmt(JmmNode assignStmt, Void unused) {
+        JmmNode expr = assignStmt.getChild(0);
+
+        if (expr.getKind().equals(Kind.INTEGER_LITERAL.toString()) || expr.getKind().equals(Kind.BOOLEAN_LITERAL.toString())) {
+            this.globalConstants.put(assignStmt.get("name"), expr);
+        }
+
+        return visit(expr);
+    }
+
+    private boolean visitVarRefExpr(JmmNode varRefExpr, Void unused) {
+        String name = varRefExpr.get("name");
+
+        if (this.globalConstants.containsKey(name)) {
+            varRefExpr.replace(this.globalConstants.get(name));
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean defaultVisit(JmmNode node, Void unused) {
+        boolean propagated = false;
+
+        for (JmmNode child : node.getChildren()) {
+            propagated = visit(child) || propagated;
+        }
+
+        return propagated;
+    }
+/*
     private boolean visitMethodDecl(JmmNode methodDecl, Void unused) {
         this.forbidden = new HashSet<>();
         this.propagatableNodes = new ArrayList<>();
@@ -103,17 +224,17 @@ public class OllirConstantPropagationVisitor extends AJmmVisitor<Void, Boolean> 
         }
 
         return changes;
-    }
+    }*/
 
 
     /**
      * Default visitor. Visits every child node and return an empty string.
      */
-    private boolean defaultVisit(JmmNode node, Void unused) {
+    /*private boolean defaultVisit(JmmNode node, Void unused) {
         boolean changes = false;
         for (var child : node.getChildren()) {
             changes = visit(child) || changes;
         }
         return changes;
-    }
+    }*/
 }
